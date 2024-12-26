303d79f7f1b4aaea1b1c37793880bc67875eeffb
Shubhamchandola <71970435+Shubhamchandola@users.noreply.github.com>
2024-12-26 13:01:23 +0530

    Add files via upload

---
+++ Project for Reliance Grocery mart/python_and_angular_files/Main URL Configuration_Reliance_mart.py
@@ -0,0 +1,6 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Use SQLite for simplicity or change to PostgreSQL/MySQL
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
